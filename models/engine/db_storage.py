ff30a06bcb3d4df0ee622f68e0f6f1ab54b2f346
Teheremiti <t.tuiaiho@gmail.com>
2023-08-03 16:59:40 -0700

    Make the User table DBStorage friendly

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -41,7 +41,7 @@ class DBStorage():
             query = self.__session.query(cls).all()
         else:
             query = []
-            classes = [State, City]
+            classes = [State, City, User]
             for Cls in classes:
                 query.extend(self.__session.query(Cls).all())
 
