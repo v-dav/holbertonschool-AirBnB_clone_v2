af3f6ca9750a73a0db1bfa2d637173a59fb528f1
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-08-03 03:07:31 -0700

    update State class for file storage

--- models/state.py
+++ models/state.py
@@ -11,3 +11,17 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", back_populates="state",
                           cascade="all, delete")
+
+    @property
+    def cities(self):
+        """Getter attribute.
+        Returns the list of City instances with state_id matching
+        the current State.id"""
+        from models import storage
+        from models.city import City
+
+        my_cities = []
+        for city in storage.all(City).values():
+            if self.id == city.state_id:
+                my_cities.append(city)
+        return my_cities
