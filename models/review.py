78946c182c23d0b2ac0a8499eb689bb37f2291b9
Teheremiti <t.tuiaiho@gmail.com>
2023-08-03 17:59:06 -0700

    Correct typo

--- models/review.py
+++ models/review.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 
-class Review(BaseModeli, Base):
+class Review(BaseModel, Base):
     """ Review class to store review information """
     __tablename__ = 'reviews'
     text = Column(String(1024), nullable=False)
