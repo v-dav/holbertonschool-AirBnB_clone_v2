8a883de5e53c29e62b2bcbdd85e7ec6e7e8e417e
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-08-16 07:19:13 -0700

    refix my code

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Web App with Flask"""
 from flask import Flask, render_template
-from models import *
+from models import storage
 from models.state import State
 from models.city import City
 
