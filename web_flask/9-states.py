5d571369bed2569c1bd1ee7b89d6a1dd657229a5
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-08-16 08:57:21 -0700

    a web app and template with conditional display

---
+++ web_flask/9-states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Web App with Flask"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+
+
+@app.route("/states", strict_slashes=False)
+def display_states():
+    """Displays states"""
+    states = storage.all(State)
+    return render_template("9-states.html", states=states,
+                           current_route="route_states")
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def cities_of_state(id):
+    """Get cities by states"""
+    list_states = storage.all(State)
+    for state in list_states.values():
+        if state.id == id:
+            cities = state.cities
+            return render_template('9-states.html', states=list_states,
+                                   cities=cities, state=state,
+                                   current_route="route_cities")
+    return render_template('9-states.html', current_route="not found")
+
+
+@app.teardown_appcontext
+def remove_sqlsession(exception=None):
+    """A method that removes SQL Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
