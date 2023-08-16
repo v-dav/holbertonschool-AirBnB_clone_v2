a64786bee0c4c62981804fe2b5f1e896013ce01d
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-08-16 07:12:00 -0700

    try to pass the last check

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
-"""A Flask Web App that renders a page with states and cities"""
-
+"""Web App with Flask"""
 from flask import Flask, render_template
-from models import storage
-from models.engine.db_storage import DBStorage
-from models.engine.file_storage import FileStorage
+from models import *
 from models.state import State
 from models.city import City
 
 app = Flask(__name__)
-states = storage.all(State)
-cities = storage.all(City)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def display_states():
-    """Displays states"""
-    return render_template("8-cities_by_states.html",
-                           st=states, cities=cities)
+@app.route('/cities_by_states', methods=['GET'])
+def cities_by_states():
+    """Get cities by states"""
+    list_states = storage.all(State)
+    list_cities = storage.all(City)
+    return render_template('8-cities_by_states.html', cities=list_cities,
+                           states=list_states)
 
 
 @app.teardown_appcontext
-def remove_sqlsession(exception=None):
-    """A method that removes SQL Session"""
+def teardown(err):
+    """remove the current session"""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
