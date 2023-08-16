fe5802934c394e701a562cdefcd03ce74fa95773
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-08-16 01:56:56 -0700

    Add a public method def close(self)

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -61,3 +61,7 @@ class FileStorage:
         if obj is not None:
             key = obj.to_dict()['__class__'] + '.' + obj.id
             del FileStorage.__objects[key]
+
+    def close(self):
+        """Deserialize JSON file to objects"""
+        self.reload()
