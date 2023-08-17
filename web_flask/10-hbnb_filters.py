7affa669da508854a38a746094a8097fcd5e6610
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-08-16 22:49:45 -0700

    create files and make first modifications

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Web App with Flask"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+
+app = Flask(__name__)
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    """Display HBNB filters"""
+    return render_template("")
+
+
+@app.teardown_appcontext
+def remove_sqlsession(exception=None):
+    """A method that removes SQL Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
